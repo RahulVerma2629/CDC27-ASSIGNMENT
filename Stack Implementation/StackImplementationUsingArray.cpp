da9fab86a2999454586563ebc3c500981d3ff031
Rahul Verma <167573663+RahulVerma2629@users.noreply.github.com>
2025-12-27 01:57:25 +0530

    Update StackImplementationUsingArray.cpp

--- Stack Implementation/StackImplementationUsingArray.cpp
+++ Stack Implementation/StackImplementationUsingArray.cpp
@@ -1,1 +1,50 @@
 
+#include <bits/stdc++.h>
+using namespace std;
+
+#define STACK_MAX_SIZE 101
+char stackArray[STACK_MAX_SIZE];
+int stackTop = -1;
+
+bool isStackEmpty() {
+    return stackTop == -1;
+}
+
+bool isStackFull() {
+    return stackTop >= STACK_MAX_SIZE - 1;
+}
+
+void pushToStack(char element) {
+    if(isStackFull()){
+        cout<<"Stack is full\n";
+        return;
+    }
+    stackArray[++stackTop] = element;
+}
+
+char popFromStack() {
+    if(isStackEmpty()){
+        cout<<"Stack is empty\n";
+        return '-1';
+    }
+    return stackArray[stackTop--];
+}
+
+int main() {
+    string inputString = "Hello, World!";
+    int inputLength = inputString.length();
+
+    // Push each character onto the stack
+    for (int i = 0; i < inputLength; i++) {
+        char currentChar = inputString[i];
+        pushToStack(currentChar);
+    }
+
+    // Pop the characters from the stack to construct the reversed string
+    string reversedString;
+    while (!isStackEmpty()) {
+        reversedString.push_back(popFromStack());
+    }
+    cout << reversedString << "\n";
+    return 0;
+}
