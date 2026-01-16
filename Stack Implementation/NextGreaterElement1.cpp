1bab4b91f5ba16e3125982e7bf7d6bb3a02050c6
Rahul Verma <167573663+RahulVerma2629@users.noreply.github.com>
2026-01-17 00:13:15 +0530

    Update NextGreaterElement1.cpp

--- Stack Implementation/NextGreaterElement1.cpp
+++ Stack Implementation/NextGreaterElement1.cpp
@@ -21,3 +21,29 @@ public:
         return ans;
     }
 };
+
+
+// Another Solution 
+class Solution {
+public:
+    vector<int> nextGreaterElement(vector<int>& nums1, vector<int>& nums2) {
+        vector<int> ans;
+        int n = nums1.size();
+        int m = nums2.size(); 
+        for(int i=0; i<n; i++){
+            for(int j=0; j<m; j++){
+                if(nums1[i] == nums2[j]){
+                    for(int k=j; k<m; k++){
+                        if(nums2[k] > nums2[j]){
+                            ans.push_back(nums2[k]);
+                            break;
+                        }
+                        if(nums2[k] <= nums2[j] && k==m-1) ans.push_back(-1); 
+                    }
+                    break;
+                }
+            }
+        }
+        return ans;
+    }
+};
